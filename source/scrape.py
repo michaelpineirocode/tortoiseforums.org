4e28eab9ee40876c56e651a3595bded641495750
Michael Pineiro <68714417+michaelpineirocode@users.noreply.github.com>
2020-12-03 10:14:35 -0700

    Update scrape.py
    Added comments and removed the "parent" option from the directory object(not needed as of now)

--- source/scrape.py
+++ source/scrape.py
@@ -10,11 +10,11 @@ class Website:
         self.content = BeautifulSoup(requests.get(self.url).text, "html.parser")
 
 class Directory:
-    def __init__(self, root, name, code=None, parent=None):
+    def __init__(self, root, name, code=None):
         self.root = root
         self.subs = []
-        self.name = name
-        self.code = code
+        self.name = name #specific name for referencing
+        self.code = code #can assign the code if neccesary, but not neccessary
         os.mkdir(self.root)
     def add_subdirectory(self, name, code=None):
         self.subdirectory = Directory(self.root + "/" + name, name, code=code)
@@ -28,14 +28,20 @@ directory = Directory("T:/" + website.name + " (" + str(uuid.uuid4())[:8] + ")",
 
 print("Scraping " + website.name)
 blocks = website.content.find_all(class_="block-container")
-for block in blocks:
-    if block.find(class_="block-header--left") != None:
-        name = block.find(class_="block-header--left").get_text().replace("!", "")[1:-1]
-        directory.add_subdirectory(name, block)
+for block in range(len(blocks)):
+    if blocks[block].find(class_="block-header--left") != None: #if there is a a block header
+        name = str(block) + " " + blocks[block].find(class_="block-header--left").get_text().replace("!", "")[1:-1]
+        directory.add_subdirectory(name, blocks[block]) #adds a subdirectory of the name, and the code at the block index
+del blocks #I don't know if this works, but I am trying to free up memory
 
-for z in range(len(directory.list_subs())):
-    block = directory.list_subs()[z].code
-    topic = block.find_all(class_="node-body")
+for z in range(len(directory.list_subs())):  
+    block = directory.list_subs()[z]
+    topic = block.code.find_all(class_="node-body") #find all the topic names from within the code
     for t in range(len(topic)):
         title = str(t + 1) + " " + topic[t].find(class_="node-title").get_text().replace("\n", "").replace("?", "")
-        print(title)
+        block.add_subdirectory(title) #add a subdirectory to the subdirectory.
+    '''
+    #An example of how a subdirectory can be referenced from a subdirectory and display the "root"(path)
+    for i in range(len(block.list_subs())):
+        print(block.list_subs()[i].root)
+    '''
