7c0757bc09594a81450a8ac30a81b3d57db9af1d
Michael Pineiro <68714417+michaelpineirocode@users.noreply.github.com>
2020-12-03 16:53:19 -0700

    Update scrape.py
    Added a function that checks if the following link has multiple topics or goes straight to a forum.
    
    I am so sick of this project but I must get it done! The dopamine from overcoming the obstacles is getting to me.
    
    It feels weird not to use functional programming, but I think that this is much easier.
    
    Also, what a NOT pythonic way to write this right? But if it works it works. It's doing what the recursive version did except 3x faster and with half as much code.

--- source/scrape.py
+++ source/scrape.py
@@ -23,25 +23,50 @@ class Directory:
     def list_subs(self):
         return self.subs
 
+def checkLink(link):
+    content = BeautifulSoup(requests.get(link).text, "html.parser")
+    blocks = content.find_all(class_="block-container")
+    
+    for b in blocks:
+        if b.find_all(class_="node-body") != []:
+            return True
+        else:
+            return False        
+
 website = Website("https://www.tortoiseforum.org/", "Tortoise Forum")
 directory = Directory("T:/" + website.name + " (" + str(uuid.uuid4())[:8] + ")", "root")
 
 print("Scraping " + website.name)
 blocks = website.content.find_all(class_="block-container")
+#creates topics
 for block in range(len(blocks)):
     if blocks[block].find(class_="block-header--left") != None: #if there is a a block header
         name = str(block) + " " + blocks[block].find(class_="block-header--left").get_text().replace("!", "")[1:-1]
         directory.add_subdirectory(name, blocks[block]) #adds a subdirectory of the name, and the code at the block index
 del blocks #I don't know if this works, but I am trying to free up memory
 
+#creates topic subdirectory
 for z in range(len(directory.list_subs())):  
     block = directory.list_subs()[z]
     topic = block.code.find_all(class_="node-body") #find all the topic names from within the code
     for t in range(len(topic)):
         title = str(t + 1) + " " + topic[t].find(class_="node-title").get_text().replace("\n", "").replace("?", "")
-        block.add_subdirectory(title) #add a subdirectory to the subdirectory.
-    '''
+        link = topic[t].find("a")["href"]
+        block.add_subdirectory(title, link) #add a subdirectory to the subdirectory.
+'''
     #An example of how a subdirectory can be referenced from a subdirectory and display the "root"(path)
     for i in range(len(block.list_subs())):
         print(block.list_subs()[i].root)
-    '''
+'''
+#goes through each topic and determines whether it is a forum or another sub
+for z in range(len(directory.list_subs())):
+    topics = directory.list_subs()[z].list_subs()
+    for i in range(len(topics)):
+        link = website.url + topics[i].code[1:]
+        print(checkLink(link))
+        if checkLink(link) == True:
+            pass
+        else:
+            pass
+        
+
